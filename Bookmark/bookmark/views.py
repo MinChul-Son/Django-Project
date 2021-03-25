e9d981ccccdd0794ee2cb1db875634bb3af9ecae
MinChul-Son <smc5236@naver.com>
2021-03-25 22:06:45 +0900

    북마크 앱 만들기 : CreateView 만들기
    제너릭 뷰인 CreateView를 사용하여 뷰를 만들고,
    template_name_suffix를 통하여 template파일 명을 지정해주었다.
    form형식으로 template을 만들고, form에 입력 성공시 redirection해줄 url을
    success_url을 통하여 앞서만든 ListView로 보내주었다.

--- Bookmark/bookmark/views.py
+++ Bookmark/bookmark/views.py
@@ -5,8 +5,17 @@ from django.shortcuts import render
 # 뷰에는 클래스형 뷰 / 함수형 뷰가 존재
 
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 from .models import Bookmark
 
 
 class BookmarkListView(ListView):
     model = Bookmark
+
+
+class BookmarkCreateView(CreateView):
+    model = Bookmark
+    fields = ['site_name', 'url']
+    success_url = reverse_lazy('list')
+    template_name_suffix = '_create'
