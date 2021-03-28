b279f8799badf9c17ba62cac428bac050561f87a
MinChul-Son <smc5236@naver.com>
2021-03-28 19:57:44 +0900

    북마크 앱 만들기 : DeleteView 만들기
    제네릭 뷰인 DeleteView를 사용하여 삭제 기능을 구현했다.
    DeleteView는 삭제를 하기때문에 모델에서의 get_absolute_url을 사용할 수
    없다. success_url메소드를 클래스 뷰 안에서 지정해주어야함.
    이제 기본적인 CRUD구현은 끝.

--- Bookmark/bookmark/views.py
+++ Bookmark/bookmark/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 # 뷰에는 클래스형 뷰 / 함수형 뷰가 존재
 
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.detail import DetailView
 
 from django.urls import reverse_lazy
@@ -31,3 +31,8 @@ class BookmarkUpdateView(UpdateView):
     model = Bookmark
     fields = ['site_name', 'url']
     template_name_suffix = '_update'
+
+
+class BookmarkDeleteView(DeleteView):
+    model = Bookmark
+    success_url = reverse_lazy('list')
