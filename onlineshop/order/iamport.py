2043e9892afa9cec09da490767ebff8ff5d106c2
MinChul-Son <smc5236@naver.com>
2021-04-11 21:03:22 +0900

    쇼핑몰 만들기 : Order 모델 생성
    주문정보를 담는 Order와 각각의 주문 상품들에 대한 정보를 담는 OrderItem
    두 모델을 만들었다.

--- onlineshop/order/iamport.py
+++ onlineshop/order/iamport.py
@@ -1,13 +1,61 @@
 import requests
+from django.conf import settings
 
 
 def get_token():  # 키와 시크릿 키를 가지고 토큰을 가져옴
-    pass
+    access_date = {
+        'imp_key': settings.IAMPORT_KEY,
+        'imp_secret': settings.IAMPORT_SECRET
+    }
+    url = "https://api.iamport.kr/users/getToken"
+    req = requests.post(url, data=access_date)
+    access_res = req.json()
+
+    if access_res['code'] is 0:
+        return access_res['response']['access_token']
+    else:
+        return None
 
 
-def payments_prepare(order_id, amount, *args, **kwargs): # 어떤 order_id로 얼마나 주문할 것인지 미리 아임포트에 알려주는 역할
-    pass
+def payments_prepare(order_id, amount, *args, **kwargs):  # 어떤 order_id로 얼마나 주문할 것인지 미리 아임포트에 알려주는 역할
+    access_token = get_token()
+    if access_token:
+        access_data = {
+            'merchant_uid':order_id,
+            'amount':amount
+        }
+        url = "https://api.iamport.kr/payments/prepare"
+        headers = {
+            'Authorization':access_token
+        }
+        req = requests.post(url, data=access_data, headers=headers)
+        res = req.json()
+        if res['code'] != 0:
+            raise ValueError("API 통신 오류")
+    else:
+        raise ValueError("토큰 오류")
 
 
 def find_transaction(order_id, *args, **kwargs):  # 결제 완료 메시지가 오면 제대로 주문이 완료되었는지 확인
-    pass
+    access_token = get_token()
+    if access_token:
+        url = "https://api.iamport.kr/payments/find/"+order_id
+        headers = {
+            'Authorization':access_token
+        }
+        req = requests.post(url, headers=headers)
+        res = req.json()
+        if res['code'] == 0:
+            context ={
+                'imp_id':res['response']['imp_uid'],
+                'merchant_order_id':res['response']['merchant_uid'],
+                'amount':res['response']['amount'],
+                'status':res['response']['status'],
+                'type':res['response']['pay_method'],
+                'receipt_url':res['response']['receipt_url']
+            }
+            return context
+        else:
+            return None
+    else:
+        raise ValueError("토큰 오류")
--- onlineshop/order/models.py
+++ onlineshop/order/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from shop.models import Product
 
 # Create your models here.
+class Order(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    address = models.CharField(max_length=250)
+    postal_code = models.CharField(max_length=20)
+    city = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    paid = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-created']
+
+    def __str__(self):
+        return f'Order {self.id}'
+
+    def get_total_product(self):
+        return sum(item.get_item_price() for item in self.items.all())
+
+    def get_total_price(self):
+        total_product = self.get_total_product()
+        return total_product
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.PROTECT, related_name='order_products')
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return str(self.id)
+
+    def get_item_price(self):
+        return self.price * self.quantity
