e42008125e172e7dc5c0193100de5813efdffd5f
MinChul-Son <smc5236@naver.com>
2021-04-16 19:07:10 +0900

    소핑몰 만들기 : TransactionManager 만들기 결제 시 바로 결제 완료를 띄우는 것이 아니라 트랜잭션에 미리 레코드를 만들어두고 결제가 완료되면 미완료에서 결제완료로 변경하는식으로 결제정보를 구성하려고한다. 이를 위해서 Manager를 만들었는데 이는 models.Manager를 사용하였다. 또한 주문번호는 해시를 이용하여 암호화하였음.

--- onlineshop/order/models.py
+++ onlineshop/order/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from shop.models import Product
 
+
 # Create your models here.
 class Order(models.Model):
     first_name = models.CharField(max_length=50)
@@ -39,3 +40,40 @@ class OrderItem(models.Model):
 
     def get_item_price(self):
         return self.price * self.quantity
+
+import hashlib
+from .iamport import payments_prepare, find_transaction
+class OrderTransactionManager(models.Manager):
+    def create_new(self, order, amount, success=None, transaction_status=None):
+        if not order:
+            raise ValueError("주문 정보 오류")
+        order_hash = hashlib.sha1(str(order.id).encode('utf-8')).hexdigest()
+        email_hash = str(order.email).split("@")[0]
+        final_hash = hashlib.sha1((order_hash+email_hash).encode("utf-8")).hexdigest()[:10]
+        merchant_order_id = str(final_hash)
+        payments_prepare(merchant_order_id, amount)
+        transaction = self.model(
+            order=order,
+            merchant_order_id=merchant_order_id,
+            amount=amount
+        )
+        if success is not None:
+            transaction.success = success
+            transaction.transaction_status = transaction_status
+
+        try:
+            transaction.save()
+        except Exception as e:
+            print("save error", e)
+        return transaction.merchant_order_id
+
+    def get_transaction(self, merchant_order_id):
+        result = find_transaction(merchant_order_id)
+        if result['status'] == 'paid':
+            return result
+        else:
+            return None
+
+
+class OrderTransaction(models.Model):
+    pass
