b95784bb1b151b9ad8d02a281fdd96bd642ae542
MinChul-Son <smc5236@naver.com>
2021-04-22 11:04:31 +0900

    쇼핑몰 만들기 : 주문페이지 만들기
    장고와 같은 최신 프레임워크에서는 확인 버튼을 눌렀을 때 같은
    페이지내에서 GET/POST를 처리한다.(페이지를 왔다갔다하는 것 자체가 리소스
    낭비)

--- onlineshop/order/views.py
+++ onlineshop/order/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
+from .models import *
+from cart.cart import Cart
+from .forms import *
 # Create your views here.
+
+def order_create(request):
+    cart = Cart(request)
+    if request.method == 'POST':
+        # 입력받은 정보를 후처리
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            for item in cart:
+                OrderItem.objects.create(order=order, product=item['product'], price=item['price'], quantity=item['quantity'])
+            cart.clear()
+            return render(request, 'order/created.html', {'order': order})
+    else:
+        # 주문자 정보를 입력받는 페이지
+        form = OrderCreateForm()
+    return render(request, 'order/create.html', {'cart': cart, 'form': form})
