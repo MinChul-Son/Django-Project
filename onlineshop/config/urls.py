77149e220d9963a86b57816d1376c8cc96f97c32
MinChul-Son <smc5236@naver.com>
2021-05-03 21:37:20 +0900

    쇼핑몰 만들기 : Order 관련 뷰 url연결
    urls.py를 만들어 Order 관련 뷰들을 연결시켰다.

--- onlineshop/config/urls.py
+++ onlineshop/config/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('cart/', include('cart.urls')),
+    path('order/', include('order.urls')),
     path('', include('shop.urls')),
 ]
