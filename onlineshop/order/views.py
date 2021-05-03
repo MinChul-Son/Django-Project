c5452952f3cc8f8ce610980f116da4dc13fa504f
MinChul-Son <smc5236@naver.com>
2021-05-03 21:10:02 +0900

    쇼핑몰 만들기 : OrderCreate, OrderCheckOut
    Ajax를 위한 주문 생성 뷰와, 주문 확인 뷰를 만들었다.

--- onlineshop/order/views.py
+++ onlineshop/order/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import *
 from cart.cart import Cart
 from .forms import *
+
+
 # Create your views here.
 
 def order_create(request):
@@ -12,10 +14,70 @@ def order_create(request):
         if form.is_valid():
             order = form.save()
             for item in cart:
-                OrderItem.objects.create(order=order, product=item['product'], price=item['price'], quantity=item['quantity'])
+                OrderItem.objects.create(order=order, product=item['product'], price=item['price'],
+                                         quantity=item['quantity'])
             cart.clear()
             return render(request, 'order/created.html', {'order': order})
     else:
         # 주문자 정보를 입력받는 페이지
         form = OrderCreateForm()
     return render(request, 'order/create.html', {'cart': cart, 'form': form})
+
+
+def order_complete(request):
+    order_id = request.GET.get('order_id')
+    # order = Order.objects.get(id=order_id)
+    order = get_object_or_404(Order, id=order_id)
+    return render(request, 'order/created.html', {'order': order})
+
+
+from django.views.generic.base import View
+from django.http import JsonResponse
+
+
+class OrderCreateAjaxView(View):
+    def post(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return JsonResponse({"authenticated": False}, status=403)
+        cart = Cart(request)
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            for item in cart:
+                OrderItem.objects.create(order=order, product=item['product'], price=item['price'],
+                                         quantity=item['quantity'])
+            cart.clear()
+            data = {
+                "order_id": order.id
+            }
+            return JsonResponse(data)
+        else:
+            return JsonResponse({}, status=401)
+
+
+class OrderCheckoutAjaxView(View):
+    def post(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return JsonResponse({"authenticated": False}, status=403)
+
+        order_id = request.POST.get('order_id')
+        order = Order.objects.get(id=order_id)
+        amount = request.POST.get('amount')
+
+        try:
+            merchant_order_id = OrderTransaction.objects.create_new(
+                order=order,
+                amount=amount
+            )
+        except:
+            merchant_order_id = None
+
+        if merchant_order_id is not None:
+            data = {
+                "works": True,
+                "merchant_id": merchant_order_id
+            }
+            return JsonResponse(data)
+
+        else:
+            return JsonResponse({}, status=401)
