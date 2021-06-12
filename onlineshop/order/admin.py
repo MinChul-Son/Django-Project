4a82b6da03c535e45dd8d5e78754ef62a4564057
MinChul-Son <smc5236@naver.com>
2021-06-12 15:25:40 +0900

    쇼핑몰 만들기 : 결제완료 페이지 추가
    created.html 추가

--- onlineshop/order/admin.py
+++ onlineshop/order/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from .models import Order, OrderItem
+from .models import OrderItem, Order
 
 
-# Register your models here.
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ['product']
@@ -12,8 +11,8 @@ class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email', 'address', 'postal_code', 'city', 'paid', 'created',
                     'updated']
     list_filter = ['paid', 'created', 'updated']
-    inlines = [OrderItemInline]
-    # actions = []
+    inlines = [OrderItemInline]  # 다른 모델과 연결되어있는 경우 한페이지 표시하고 싶을 때
+    # actions = [export_to_csv]
 
 
 admin.site.register(Order, OrderAdmin)
