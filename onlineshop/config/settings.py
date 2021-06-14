0f42a450a2dbc9997b7c5afb04431ceacd418499
MinChul-Son <smc5236@naver.com>
2021-06-14 13:00:24 +0900

    쇼핑몰 만들기 : aws ec2를 사용하여 배포
    aws를 사용하여 클라우드 서버에 배포를 진행해보았다.
    마무리 수정하고 백그라운드로 실행시켜놓으면 될듯

--- onlineshop/config/settings.py
+++ onlineshop/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = my_settings.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.compute.amazonaws.com', '127.0.0.1', 'localhost']
 
 # Application definition
 
--- onlineshop/shop/views.py
+++ onlineshop/shop/views.py
@@ -22,5 +22,19 @@ def product_in_category(request, category_slug=None):
 
 def product_detail(request, id, product_slug=None):
     product = get_object_or_404(Product, id=id, slug=product_slug)
-    add_to_cart = AddProductForm(initial={'quantity':1})
+    add_to_cart = AddProductForm(initial={'quantity': 1})
     return render(request, 'shop/detail.html', {'product': product, 'add_to_cart': add_to_cart})
+
+
+# def search_list(request):
+#     products = Product.objects.all()
+#
+#     search_key = request.GET.get('search_key')  # 검색어 가져오기
+#     if search_key:  # 만약 검색어가 존재하면
+#         products = products.filter(name__contains=search_key)  # 해당 검색어를 포함한 queryset 가져오기
+#     print(products)
+#     return render(request, 'shop/list.html',
+#                   {
+#                       'products': products,
+#                       'search_key': search_key
+#                   })
